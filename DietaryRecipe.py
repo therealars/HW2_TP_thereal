299ccb818d93dac7d8c3efd0b3d2466f22a886df
therealars <7192209@mail.ru>
2026-06-03 20:23:05 +0300

    Закомитил класс DietaryRecipe ура остались тесты

---
+++ DietaryRecipe.py
@@ -0,0 +1,13 @@
+from Recipe import Recipe
+
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients=None):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+    def scale(self, ratio: float):
+        scaled_recipe = super().scale(ratio)
+        return DietaryRecipe(scaled_recipe.title, self.diet_type, scaled_recipe.ingredients)
+    
+    def __str__(self):
+        return f'[{self.diet_type}] {super().__str__()}'
